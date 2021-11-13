f931b065ddcec3f80a7076a5f7faa69d5715b1bf
latha n <latha9510@gmail.com>
2021-11-13 20:50:54 +0530

    my changes

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,88 +1,87 @@
-import React from 'react'
-import jack from '../jack.jpg'
-import jeans from '../images/jeans.png'
+    import React, { useState } from 'react'
+    import jeans from '../images/jeans.png'
 
-const Sidebar = () => {
-  return (
-    <div class="container-fluid bg-white">
-        
-            <div class="card img-fluid jens">
-            <img src={jeans} width="100%"/>
-            </div>
+    const Sidebar = () => {
+        const [collections,setCollection] = useState(['Men','Women','Kids','Footwear','Accesories'])
+        const [categories,setCategories] = useState(['All Collections','Dresses','JumpSuits','Winter Wear','Tops'])
+        const [occation,setOccation] = useState(['All Collections','Casual','Formal','Party'])
+        const [colors,setColors] = useState(['0080ff','ffffff','#cc0099','#ff80bf','#339966','#ff6088','#dd0567','#ff0099','#gg9060','#33ff33','#ff9900','#ff3020'])
+        const [stock,setStock] = useState(['All','New','Old'])
 
-            <div class="border-bottom  ">
-                <h1 style={{marginLeft:"1rem"}}>Collections</h1>
-                <form style={{marginLeft:"1rem"}}>
-                <input type="checkbox"/> Men <br/>
-                <input type="checkbox"/> Women <br/>
-                <input type="checkbox"/> Kids <br/>
-                <input type="checkbox"/> Footwear <br/>
-                <input type="checkbox"/> Accessories <br/>
+    return (
+        <div class="container-fluid bg-white">
+            
+                <div class="card img-fluid jens">
+                <img src={jeans} width="100%"/>
+                </div>
+
+                <div class="border-bottom  ">
+                    <h1>Collections</h1>
+                    <form>
+                        {
+                            collections.map((coll)=>{
+                                return <span><input type="checkbox" class="small"/>{coll}<br/></span>
+                            })
+                        }
+                    </form>
+                </div>
+
+                <div class="border-bottom   ">
+                    <h1>Cotegories</h1>
+                    <form>
+                        {
+                            categories.map((cat)=>{
+                                return <span><input type="checkbox" class="small"/>{cat}<br/></span>
+                            })
+                        }
+
+                    </form>
+                    
+                </div>
+
+                <div class="border-bottom  ">
+                    <h1>Occation</h1>
+                    <form>
+                    {
+                        occation.map((occ)=>{
+                            return <span><input type="checkbox" class="small"/>{occ}<br/></span>
+                        })
+                    }
+                    </form>
+                </div>
+
+                <div class="dropdown ">
+                    <h1>Stories</h1>
+                    <button class="btn btn-white dropdown-toggle border small" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    Show White
+                    </button>
+                </div>
+
+                <div>
+                    <h1>Colors</h1>
+                    <form>
+                        {
+                            colors.map((colo)=>{
+                                return <span><input type="color" value={colo}/></span>
+                            })
+                        }
+                        <span>+24 more</span>
                 </form>
-            </div>
-            <div class="border-bottom   ">
-                <h1 style={{marginLeft:"1rem"}}>Cotegories</h1>
-                <form style={{marginLeft:"1rem"}}>
-                <input type="checkbox"/> All Collections <br/>
-                <input type="checkbox"/> Dresses <br/>
-                <input type="checkbox"/> Jumpsuits <br/>
-                <input type="checkbox"/> Winter Wear <br/>
-                <input type="checkbox"/> Tops<br/>
-                </form>
-                
-            </div>
-            <div class="border-bottom  ">
-                <h1 style={{marginLeft:"1rem"}}>Occation</h1>
-                <form style={{marginLeft:"1rem"}}>
-                <input type="checkbox"/> All Collections<br/>
-                <input type="checkbox"/> Casual <br/>
-                <input type="checkbox"/> Formal <br/>
-                <input type="checkbox"/> Party <br/>
-                </form>
-            </div>
-            <div class="dropdown ">
-                <h1 style={{marginLeft:"1rem"}}>Stories</h1>
-                <button class="btn btn-white dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style={{marginLeft:"1rem"}}>
-                 Show White
-                </button>
-            </div>
-            <div>
-                <h1 style={{marginLeft:"1rem"}}>Colors</h1>
-                <form>
-                <input type="color" value="#0080ff"/>                
-                <input type="color" value="#ffffff"/>
-                <input type="color" value="#cc0099"/>
-                <input type="color" value="#ff80bf"/>
-                <input type="color" value="#339966"/>
-                <input type="color" value="#ff6088"/>
-                <input type="color" value="#dd0567"/>
-                <input type="color" value="#ff0099"/>
-                <input type="color" value="#gg9060"/>
-                <input type="color" value="#33ff33"/>
-                <input type="color" value="#ff9900"/>
-                <input type="color" value="#ff3020"/>
 
-            </form>
-
+                </div>
+                <div>
+                    <h1>Stock</h1>
+                    <form>
+                        {
+                            stock.map((sto)=>{
+                                return <span><input type="radio" class="small"/>{sto}<br/></span>
+                            })
+                        }
+                    </form>
+                </div>
             </div>
-            <div>
-                <h1 style={{marginLeft:"1rem"}}>Stock</h1>
-                <form style={{marginLeft:"1rem"}}>
-                <input type="radio"/> All<br/>
-                <input type="radio"/>New <br/>
-                <input type="radio" />old <br/>
-                </form>
-            </div>
-    
-    
-    
-    </div>
 
+        )
+    }
 
-
-      
-      
-  )
-}
-
-export default Sidebar
+    export default Sidebar
--- src/Components/TopNavBar.js
+++ src/Components/TopNavBar.js
@@ -10,158 +10,54 @@ import logo2 from '../logo2.png'
 const TopNavbar = () => {
 
   return (
-
-    
-    
-//     <div>
-//         <nav className="navbar navbar-expand-sm" style={{background:'#fff',borderBottom:' 2px solid #dccccc45', display:'flex',flexDirection:'row',justifyContent:'space-between'}}>
+        <div>
+          <nav class="navbar navbar-expand-lg navbar-light  nav" >
+          <a class="navbar-brand" href="#">
+                <div class="navd">
+                  <img src={intrakraft} width="60px" height="60px" />
+                      <div class="navdd">
+                          <h2 class="fs-2 opacity-100 fw-bold">Intrakarft</h2>
+                        <p class="fs-6 opacity-50">Correct. Interact. Transact.</p>            </div>
+                </div>
+              </a>
             
-//         <a className="navbar-brand" href="#" style={{listStyle:'none',color:'black'}}>
-//       <div style={{display:'flex',flexDirection:'row'}}>
-//        <img src={intrakraft} width="60px" height="60px" />
-//            <div style={{display:'flex',flexDirection:'column',marginLeft:'10px'}}>
-//                <h2 style={{fontWeight:'bold',opacity:1}}>Intrakarft</h2>
-//                <p style={{fontSize:'14px',opacity:0.8}}>Correct. Interact. Transact.</p>
-//            </div>
-//       </div>
-//    </a>
-//    <ul className="navbar-nav" >
-//        <li className="nav-item mr-5">
-//            <a style={{fontSize:'20px',fontWeight:550,opacity:1}} className="nav-link text-dark" href="#">Orders</a>
-//        </li>
-//        <li className="nav-item active mr-5">
-//            <a style={{fontSize:'20px',fontWeight:550,opacity:1}} className="nav-link  text-dark" href="#">Brands</a>
-//        </li>
-//        <li className="nav-item mr-5">
-//            <a style={{fontSize:'20px',fontWeight:550,opacity:1}} className="nav-link  text-dark" href="#">Connections</a>
-//        </li>
-      
-//        <li className="nav-item mr-5">
-//            <a className="nav-link" href="#">
-//                <FontAwesomeIcon icon={faShoppingBag} style={{fontSize:'30',color:'black',opacity:0.3}}/>
-//            </a>
-//        </li>
-//        <li className="nav-item mr-5">
-//            <a className="nav-link d-flex justify-content-start align-items-start" href="#">
-//                <FontAwesomeIcon icon={faBell} style={{fontSize:'30',color:'black',opacity:0.3}}/>
-//                <span class="badge badge-warning text-white">1</span>
-//            </a>
-//        </li>
-//        <li>
-//        <div style={{display:'flex',flexDirection:'row'}}>
-//        <img src={logo2} width="60px" height="60px" />
-//            <div class="dropdown" style={{display:'flex',flexDirection:'column',marginLeft:'0px'}}>
-//                <h2  class="btn  dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"style={{fontWeight:'bold',opacity:1}}>Welcome<br/> shopper stop</h2>
-//            </div>
-//       </div>
-//     </li>
-      
-//    </ul>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <ul class="navbar-nav" id="navbarSupportedContent">
+                  <li class="nav-item ml-5">
+                      <a style={{fontSize:'20px',fontWeight:550,opacity:1}} class="nav-link text-dark" href="#">Orders</a>
+                </li>
+                <li class="nav-item active mr-5 ml-5">
+                      <a style={{fontSize:'20px',fontWeight:550,opacity:1}} class="nav-link  text-dark" href="#">Brands</a>
+                  </li>
+                  <li class="nav-item mr-5">
+                      <a style={{fontSize:'20px',fontWeight:550,opacity:1}} class="nav-link  text-dark" href="#">Connections</a>
+                  </li>
+                
+                  <li class="nav-item mr-5">
+                      <a class="nav-link" href="#">
+                          <FontAwesomeIcon icon={faShoppingBag} style={{fontSize:'30',color:'black',opacity:0.3}}/>
+                      </a>
+                  </li>
+                <li class="nav-item mr-5">
+                      <a class="nav-link d-flex justify-content-start align-items-start" href="#">
+                          <FontAwesomeIcon icon={faBell} style={{fontSize:'30',color:'black',opacity:0.3}}/>
+                          <span class="badge badge-warning text-white">1</span>
+                      </a>
+                  </li>
+                  <li class="navbar-item  ">
+                    <a class="border-left nav-link border-left"><img class="rounded-circle" src={logo2} width="60px" height="60px" /></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                            <button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown"> <strong>Welcome <br/>Shopper shope</strong> </button>
+                            <div class="dropdown-menu"> <a class="dropdown-item" href="#">Link 1</a> <a class="dropdown-item" href="#">Link 2</a> <a class="dropdown-item" href="#">Link 3</a> </div>
+                      </li>
+                </ul>
+          </nav>
+          </div>
 
-// </nav> 
-// </div>
-
-<div>
-<nav class="navbar navbar-expand-lg navbar-light  nav" >
-<a class="navbar-brand" href="#">
-       <div class="navd">
-        <img src={intrakraft} width="60px" height="60px" />
-            <div class="navdd">
-                <h2 class="fs-2 opacity-100 fw-bold">Intrakarft</h2>
-               <p class="fs-6 opacity-50">Correct. Interact. Transact.</p>            </div>
-       </div>
-    </a>
-  {/* <div class="container-fluid">
-  
-  <img src={intrakraft} width="60px" height="50px" />
-  <div style={{display:'flex',flexDirection:'column',marginLeft:'10px'}}>
-               <h2 style={{fontWeight:'bold',opacity:1}}>Intrakarft</h2>
-                <p style={{fontSize:'14px',opacity:0.8}}>Correct. Interact. Transact.</p>
-    </div> */}
-    {/* <a class="navbar-brand" href="#" style={{listStyle:'none',color:'black'}}>Intrakarft</a> */}
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    {/* <div class="collapse navbar-collapse" id="navbarSupportedContent" > */}
-    <ul class="navbar-nav" id="navbarSupportedContent">
-        <li class="nav-item ml-5">
-            <a style={{fontSize:'20px',fontWeight:550,opacity:1}} class="nav-link text-dark" href="#">Orders</a>
-       </li>
-       <li class="nav-item active mr-5 ml-5">
-            <a style={{fontSize:'20px',fontWeight:550,opacity:1}} class="nav-link  text-dark" href="#">Brands</a>
-        </li>
-        <li class="nav-item mr-5">
-            <a style={{fontSize:'20px',fontWeight:550,opacity:1}} class="nav-link  text-dark" href="#">Connections</a>
-        </li>
-      
-        <li class="nav-item mr-5">
-            <a class="nav-link" href="#">
-                <FontAwesomeIcon icon={faShoppingBag} style={{fontSize:'30',color:'black',opacity:0.3}}/>
-            </a>
-        </li>
-       <li class="nav-item mr-5">
-            <a class="nav-link d-flex justify-content-start align-items-start" href="#">
-                <FontAwesomeIcon icon={faBell} style={{fontSize:'30',color:'black',opacity:0.3}}/>
-                <span class="badge badge-warning text-white">1</span>
-            </a>
-        </li>
-        <li class="navbar-item  ">
-           <a class="border-left nav-link border-left"><img class="rounded-circle" src={logo2} width="60px" height="60px" /></a>
-           </li>
-           <li class="nav-item dropdown">
-                  <button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown"> <strong>Welcome <br/>Shopper shope</strong> </button>
-                  <div class="dropdown-menu"> <a class="dropdown-item" href="#">Link 1</a> <a class="dropdown-item" href="#">Link 2</a> <a class="dropdown-item" href="#">Link 3</a> </div>
-            </li>
-      
-    </ul>
-
-      {/* <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">orders</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Brands</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Collections</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-          <FontAwesomeIcon icon={faShoppingBag}/>
-        </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-          <FontAwesomeIcon icon={faBell}/>
-              <span class="badge badge-warning text-white">1</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">
-          <img src={logo2} width="60px"/>
-        </a>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Welcome to shopper<br/>shop
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        
-      </ul>
-        */}
-    {/* </div> */}
-  {/* </div> */}
-</nav>
-</div>
-
-    
-  )
+)
 }
 
 export default TopNavbar
